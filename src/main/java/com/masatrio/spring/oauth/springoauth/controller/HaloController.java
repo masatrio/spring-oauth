c2c327a1167cf826ecc37dda81023cbb6123b43d
masatrio <ajisatriomaulana@gmail.com>
2018-10-24 16:20:21 +0700

    add hello controller and thymeleaf package

---
+++ src/main/java/com/masatrio/spring/oauth/springoauth/controller/HaloController.java
@@ -0,0 +1,17 @@
+package com.masatrio.spring.oauth.springoauth.controller;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HaloController {
+	
+	@RequestMapping("/halo")
+	public void halo(Model m) {
+		m.addAttribute("waktu", new Date());
+	}
+
+}
